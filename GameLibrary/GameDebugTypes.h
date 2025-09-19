42fc5daba2223bff40cac5cb78ef336ea52c04f0
Javier Gomez Zaragoza <javiergomezzaragoza@gmail.com>
2025-09-20 00:44:54 +0200

    Random enums

--- GameLibrary/GameDebugTypes.h
+++ GameLibrary/GameDebugTypes.h
@@ -42,7 +42,7 @@ struct debug_enum_value {
     int Value;
 };
 
-const int ENUM_VALUES_SIZE = 25;
+const int ENUM_VALUES_SIZE = 32;
 debug_enum_value EnumValues[ENUM_VALUES_SIZE] = {
     {Debug_Type_game_asset_type, "Asset_Type_Text", 0},
     {Debug_Type_game_asset_type, "Asset_Type_Bitmap", 1},
@@ -52,23 +52,30 @@ debug_enum_value EnumValues[ENUM_VALUES_SIZE] = {
     {Debug_Type_game_asset_type, "Asset_Type_Video", 5},
     {Debug_Type_game_asset_type, "Asset_Type_Mesh", 6},
     {Debug_Type_game_asset_type, "Asset_Type_Animation", 7},
+    {Debug_Type_game_asset_type, "game_asset_type_count", 8},
     {Debug_Type_game_text_id, "Text_Test_ID", 0},
+    {Debug_Type_game_text_id, "game_text_id_count", 1},
     {Debug_Type_game_heightmap_id, "Heightmap_Spain_ID", 0},
+    {Debug_Type_game_heightmap_id, "game_heightmap_id_count", 1},
     {Debug_Type_game_animation_id, "Animation_Idle_ID", 0},
     {Debug_Type_game_animation_id, "Animation_Walk_ID", 1},
     {Debug_Type_game_animation_id, "Animation_Jump_ID", 2},
     {Debug_Type_game_animation_id, "Animation_Attack_ID", 3},
+    {Debug_Type_game_animation_id, "game_animation_id_count", 4},
     {Debug_Type_game_entity_type, "Entity_Type_Character", 0},
     {Debug_Type_game_entity_type, "Entity_Type_Enemy", 1},
     {Debug_Type_game_entity_type, "Entity_Type_Camera", 2},
     {Debug_Type_game_entity_type, "Entity_Type_Prop", 3},
     {Debug_Type_game_entity_type, "Entity_Type_Weapon", 4},
+    {Debug_Type_game_entity_type, "game_entity_type_count", 5},
     {Debug_Type_weapon_type, "Weapon_Sword", 0},
     {Debug_Type_weapon_type, "Weapon_Shield", 1},
+    {Debug_Type_weapon_type, "weapon_type_count", 2},
     {Debug_Type_character_action_id, "Character_Action_Idle_ID", 0},
     {Debug_Type_character_action_id, "Character_Action_Walk_ID", 1},
     {Debug_Type_character_action_id, "Character_Action_Jump_ID", 2},
     {Debug_Type_character_action_id, "Character_Action_Attack_ID", 3},
+    {Debug_Type_character_action_id, "character_action_id_count", 4},
 };
 
 struct debug_struct_member {
