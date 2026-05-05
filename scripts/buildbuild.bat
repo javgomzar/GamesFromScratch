6019df5f298d04311eaff75d6d6f33b9e93062da
Javier Gomez Zaragoza <javiergomezzaragoza@gmail.com>
2026-05-06 00:47:27 +0200

    Unnecessary pdb label at buildbuild release mode

--- scripts/buildbuild.bat
+++ scripts/buildbuild.bat
@@ -7,4 +7,4 @@ call scripts\env.bat
 @REM %COMPILE% /W0 /nologo /std:c++20 /Fo"bin\Build.obj" /Fd"bin\Build.pdb" GameBuild\GameBuild.cpp /D _DEBUG /EHsc /MDd /Zi /Od /fsanitize=address /link /OUT:"bin\Build.exe"
 
 @REM PERFORMANCE COMPILE
-%COMPILE% /W0 /nologo /std:c++20 /Fo"bin\Build.obj" /Fd"bin\Build.pdb" GameBuild\GameBuild.cpp /O2 /link /OUT:"bin\Build.exe"
+%COMPILE% /W0 /nologo /std:c++20 /Fo"bin\Build.obj" GameBuild\GameBuild.cpp /O2 /link /OUT:"bin\Build.exe"
